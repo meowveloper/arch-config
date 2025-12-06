f36c73d3b1d7470b5efc3993dbcbee068aec2e7b
meowveloper <meowveloper.dev@gmail.com>
2025-12-07 06:26:52 +0700

    fish config

--- chezmoi/dank/dot_config/fish/config.fish
+++ chezmoi/dank/dot_config/fish/config.fish
@@ -6,6 +6,11 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #    # smth smth
 #end
 
+for file in ~/.config/shell/{profile,aliases,interactive} ~/.config/shell/*.bash; do
+    [[ -r "$file" ]] && source "$file"
+done
+
+
 function y
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
@@ -18,3 +23,8 @@ end
 function fish_greeting
 	# nothing
 end
+
+# zoxide (smarter cd)
+if command -v zoxide >/dev/null 2>&1; then
+    eval "$(zoxide init bash)"
+fi
