130a74f683b46c36c87fbd0de924ad79fc7e33a3
meowveloper <meowveloper.dev@gmail.com>
2025-12-07 05:00:43 +0700

    fish config

---
+++ chezmoi/dank/dot_config/fish/config.fish
@@ -0,0 +1,16 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
