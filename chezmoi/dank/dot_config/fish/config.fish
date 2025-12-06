d7b811e5b40761c1d08bb1139251ec92b715a2a5
meowveloper <meowveloper.dev@gmail.com>
2025-12-07 06:41:57 +0700

    fish config

--- chezmoi/dank/dot_config/fish/config.fish
+++ chezmoi/dank/dot_config/fish/config.fish
@@ -1,14 +1,26 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+alias ls='eza -l --color=auto --group-directories-first --icons=auto'
+alias la='eza -la --color=auto --group-directories-first --icons=auto'
+alias l='eza -laa --color=auto --group-directories-first --icons=auto'
+alias tree='eza --tree'
 
-for file in ~/.config/shell/{profile,aliases,interactive} ~/.config/shell/*.bash; do
-    [[ -r "$file" ]] && source "$file"
-done
+alias cat='bat --style=plain'
+alias grep='rg'
+alias find='fd'
+
+alias cd='z'
+alias zi='z -i'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias df='duf'
+alias top='btop'
+
+
+export EDITOR=nvim
+export VISUAL=nvim
+export SUDO_EDITOR=nvim
 
 
 function y
@@ -26,3 +38,6 @@ end
 
 
 zoxide init fish | source
+
+source ~/.config/shell/user
+
