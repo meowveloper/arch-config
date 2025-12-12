7cae929ef0b5d5712426c736b1065f3431b4eed7
meowveloper <meowveloper.dev@gmail.com>
2025-12-13 03:13:05 +0700

    desktop portal

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
 echo "running flatpak post install hook"
+systemctl --user enable --now xdg-desktop-portal-gnome
 flatpak uninstall --unused
 echo "flatpak post install hook done"
+
